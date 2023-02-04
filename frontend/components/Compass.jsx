52cfb68412c490b6edef251d9c049f84e4d067bf
Andrew Holt <amholt@usc.edu>
2023-02-04 12:54:01 -0800

    basic compass

--- frontend/components/Compass.jsx
+++ frontend/components/Compass.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {
+    Box,
+    Center,
     NativeBaseProvider,
 } from 'native-base'
 
 const Compass = () => {
     return (
         <Box style={{borderColor: 'gray', borderWidth: 5, borderStyle: 'dotted', borderRadius: 200, width: 325, height: 325}}>
-            <Box style={{borderColor: 'gray', borderWidth: 5, borderStyle: 'dotted', borderRadius: 200, width: 325, height: 325}}>
-            </Box>
+            <Center>
+                <Box style={{height: 25, width: 0, borderColor: 'red', borderWidth: 1}} />
+            </Center>
         </Box>
     )
 }
