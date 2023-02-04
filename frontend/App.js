04cac47917fdd9fba8dba0cd97c6a45f13171a98
e-sawyer <109112191+ewsawyer@users.noreply.github.com>
2023-02-04 15:33:11 -0800

    working compass app!!

--- frontend/App.js
+++ frontend/App.js
@@ -15,6 +15,8 @@ import {
 } from "native-base"
 import NativeBaseIcon from "./components/NativeBaseIcon"
 import { Platform } from "react-native"
+import * as Location from "expo-location"
+import { useState, useEffect } from "react"
 
 // components
 import Compass from './components/Compass'
@@ -29,6 +31,40 @@ const config = {
 export const theme = extendTheme({ config })
 
 const App = () => {
+  
+  const [heading, setHeading] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      
+      console.log('async function called.')
+      
+      // check if user has allowed us to use their location while app is in the foreground
+      const permissions = await Location.getForegroundPermissionsAsync();
+    
+      console.log('permissions checked:', permissions)
+
+      // user has not allowed us to use their location
+      if(!permissions.granted) {
+        console.log('Permissions not granted. Asking now...')
+        const newPermissions = await Location.requestForegroundPermissionsAsync();
+
+        if(!newPermissions.granted) {
+          console.log('Permissions still denied! Returning...')
+          return;
+        }
+      }
+      
+      // listen for changes in the phone's compass heading
+      Location.watchHeadingAsync(data => {
+        let { trueHeading } = data;
+        trueHeading = Math.round(trueHeading);
+        setHeading(trueHeading);
+      })
+      .catch(err => console.log(err));
+    })()
+  }, []);
+  
   return (
     <NativeBaseProvider>
       {/* <Center
@@ -65,8 +101,9 @@ const App = () => {
           </Link>
         </VStack>
       </Center> */}
-
+      
       <Center style={{width: '100%', height: '100%', backgroundColor: 'black'}}>
+        <Text style={{color: 'white', fontSize: '30pt', lineHeight: 100 }}>{heading}°</Text>
         <Compass />
       </Center>
 
