5462a90fa2354bf5cba65cda7da5506db4518def
Paul Somodi <somodi@usc.edu>
2023-02-05 00:20:58 -0800

    added current date/time

--- frontend/App.js
+++ frontend/App.js
@@ -16,6 +16,7 @@ import {
 import NativeBaseIcon from "./components/NativeBaseIcon"
 import { Platform } from "react-native"
 import * as Location from "expo-location"
+import moment from 'moment';
 
 // components
 import Compass from './components/Compass'
@@ -35,10 +36,15 @@ const App = () => {
 
   const [azimuth, setAzimuth] = useState(0);
   const [altitude, setAltitude] = useState(0);
-  const [heading, setHeading] = useState(0)
+  const [heading, setHeading] = useState(0);
+  var year = moment().utcOffset('-08:00').format('YYYY')
+  var month = moment().utcOffset('-08:00').format('MM')
+  var day = moment().utcOffset('-08:00').format('DD') - 8
+  var hour = moment().utcOffset('-08:00').format('HH')
+  var minute = moment().utcOffset('-08:00').format('mm')
 
   useEffect(() => {
-    fetch('http://unpaul.pythonanywhere.com/planet?year=2023&month=2&day=4&hour=20&minute=31&planet=moon&lat=34.0522&lng=-118.243', {
+    fetch('http://unpaul.pythonanywhere.com/planet?year=' + year + '&month=' + month + '&day=' + day + '&hour=' + hour + '&minute=' + minute + '&planet=moon&lat=34.0522&lng=-118.243', {
       method:'GET'
     })
     .then(resp => resp.json())
@@ -56,7 +62,7 @@ const App = () => {
   return (
     <NativeBaseProvider>      
       <Center style={{width: '100%', height: '100%', backgroundColor: 'black'}}>
-        {/* <Text style={{color: 'white', fontSize: '30pt', lineHeight: 100 }}>{azimuth}°</Text> */}
+        {/* <Text style={{color: 'white', fontSize: '30pt', lineHeight: 100 }}>{year} + {month} + {day} + {hour} + {minute}°</Text> */}
         <Pointer />
         <Compass azimuth={azimuth} onHeadingChange={handleHeadingChange} />
         <Galaxy rotation={heading}/>
